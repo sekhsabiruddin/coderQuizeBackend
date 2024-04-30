2583127a6afe27bf94a79e46eba1f0e7ce197f29
unknown <sksabiruddin81@gmail.com>
2024-04-30 12:05:44 +0530

    cors plm reslove

--- app.js
+++ app.js
@@ -12,19 +12,19 @@ const Feedback = require("./controller/feedbackController");
 const Admin = require("./controller/adminController");
 const errorMiddleware = require("./middleware/error");
 // Middleware
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
-
 // app.use(
 //   cors({
-//     origin: ["https://code-quize-frontend-trkr.vercel.app"],
+//     origin: "http://localhost:5173",
 //     credentials: true,
 //   })
 // );
+
+app.use(
+  cors({
+    origin: ["https://code-quize-frontend-trkr.vercel.app"],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.json({ limit: "50mb" })); // JSON parsing with limit
 app.use(express.urlencoded({ extended: true })); // Form data parsing
